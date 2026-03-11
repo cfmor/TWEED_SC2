928d2770fe678ba8641406fc42e202ae0c4c40c7
kmruehl <ruehlkm@gmail.com>
2026-03-11 16:07:44 +0100

    generalizing plot function

--- tweed_sc2/wind_power_forecasting/plot_data.py
+++ tweed_sc2/wind_power_forecasting/plot_data.py
@@ -1,23 +1,34 @@
 import matplotlib.pyplot as plt
 
-def plot_location_power(df, save_path='power_comparison.png'):
+    
+def plot_location_data(df, col):
     """
-    Loads dataframe and plots Time vs Power for each.
+    Loads dataframe and plots Time vs datastream for each location.
     """
-
+    
     locations = df['Location'].unique()
     fig, axes = plt.subplots(len(locations), 1, figsize=(12, 4 * len(locations)), sharex=True) 
 
     for i, loc in enumerate(locations):
         loc_data = df[df['Location'] == loc]
         ax = axes[i]
-        ax.plot(loc_data['Time'], loc_data['Power'], color='tab:blue', lw=0.5)
+        ax.plot(loc_data['Time'], loc_data[col], color='tab:blue', lw=0.5)
         ax.set_title(f'{loc}', fontsize=14)
-        ax.set_ylabel('Power Output')
+        if col == 'Power':
+            ax.set_ylabel(col + ' (%)')
+        elif col == 'windspeed_10m' or col == 'windspeed_100m' or col == 'windgusts_10m':
+            ax.set_ylabel(col + ' (m/s)')
+        elif col == col == 'winddirection_10m' or col == 'winddirection_100m':
+            ax.set_ylabel(col + ' (deg)')
+        elif col == 'relativehumidity_2m':
+            ax.set_ylabel(col + ' (%)')
+        elif col == 'temperature_2m' or col == 'dewpoint_2m':
+            ax.set_ylabel(col+' (K)')
         ax.grid(True, linestyle='--', alpha=0.6)
 
     axes[-1].set_xlabel('Timestamp')
     plt.tight_layout()
 
+    save_path = col + '.png'
     plt.savefig(save_path, dpi=300)
     plt.show()
